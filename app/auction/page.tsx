26a7e5b8e5c4b7fdc074244ba86649602f8a12dd
lizzka18 <akseonowww@yandex.by>
2025-04-12 19:23:39 +0000

    Update 2#

--- app/auction/page.tsx
+++ app/auction/page.tsx
@@ -1,8 +1,8 @@
-'use client';
+"use client"
 
-import { useState } from "react";
 import LotItem from "@/components/auction/LotItem";
 import Timer from "@/components/auction/Timer";
+import { useState } from "react";
 
 type Lot = {
   id: number;
